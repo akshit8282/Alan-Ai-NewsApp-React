c6b8881beb5a92fc098ecac63e61c42c18eb1167
Akshit <akshitbatra222@gmail.com>
2021-01-09 12:32:34 +0530

    App.js file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {React,useEffect,useState} from 'react'
 import alanBtn from "@alan-ai/alan-sdk-web";
 import NewsCards from './Components/NewsCards/NewsCards'
 import useStyle from './style.js'
+import wordsToNumbers from 'words-to-numbers';
 const alnkey='4bec427bdef5108bf22d2521311f80832e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 
@@ -10,12 +11,31 @@ const alnkey='4bec427bdef5108bf22d2521311f80832e956eca572e1d8b807a3e2338fdd0dc/s
 
 function App() {
    const [newsState,setnewsState]=useState([]);
+   const [article,setarticle]=useState(-1);
+  
 useEffect(()=>{
 alanBtn({
 key:alnkey,
-onCommand:({command,articles})=>{
+onCommand:({command,articles,number,savedArticles})=>{
     if (command === 'newHeadlines') {
+        console.log(articles);
         setnewsState(articles);
+        setarticle(-1);
+    }else if(command==='highlights'){
+       
+        setarticle((prevarticle)=>(prevarticle+1));
+    }else if(command==='open'){
+        const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+          const article = savedArticles[parsedNumber - 1];
+
+          if (parsedNumber > savedArticles.length) {
+            alanBtn().playText('Please try that again...');
+          } else if (savedArticles) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          } else {
+            alanBtn().playText('Please try that again...');
+          }
     }
 }
 })
@@ -26,7 +46,7 @@ const classes=useStyle();
             <div className={classes.logoContainer}>
                 <img src=  'https://alan.app/voice/images/previews/preview.jpg' alt="aln logo" className={classes.alanLogo}/> 
             </div>
-            <NewsCards articles={newsState}/>
+            <NewsCards articles={newsState} activearticle={article}/>
         </div>
     )
 }
